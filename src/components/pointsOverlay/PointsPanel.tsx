bf357d4fc127f994b63adf4344f1bb03f9a0876b
UltraMarlin <68075515+UltraMarlin@users.noreply.github.com>
2025-09-11 01:40:45 +0200

    feat: adjust PointsPanel design

--- src/components/pointsOverlay/PointsPanel.tsx
+++ src/components/pointsOverlay/PointsPanel.tsx
@@ -11,13 +11,13 @@ export const PointsPanel = ({
 }: PointsPanelProps) => {
   return (
     <div
-      className="flex w-full max-w-[440px] flex-col items-center rounded-lg border-4 border-[color-mix(in_oklab,currentColor,white_40%)] bg-gradient-to-tr from-[currentColor] to-[color-mix(in_oklab,currentColor,white_25%)] pb-4 pt-2 text-5xl text-[--accentColor]"
+      className="flex w-full max-w-[440px] flex-col items-center rounded-lg border-4 border-[color-mix(in_oklab,currentColor,white_50%)] bg-gradient-to-t from-current to-[color-mix(in_oklab,currentColor,white_35%)] pb-3 pt-2 text-5xl text-[--accentColor]"
       style={{ "--accentColor": accentColor }}
     >
-      <div className="mb-2 font-semibold text-[color-mix(in_oklab,currentColor,black_60%)]">
+      <div className="mb-4 font-semibold text-[color-mix(in_oklab,currentColor,black_60%)]">
         {name}
       </div>
-      <div className="w-[180px] rounded-md bg-[color-mix(in_oklab,currentColor,white_60%)] px-6 py-1 text-center text-[3.5rem]">
+      <div className="w-[180px] rounded-md bg-[color-mix(in_oklab,currentColor,white_60%)] px-6 pb-2 pt-1 text-center text-[3.25rem] shadow-[inset_0_0_5px_3px_color-mix(in_oklab,currentColor,#00000049_25%)]">
         <span className="font-bold text-[color-mix(in_oklab,currentColor,black_50%)]">
           {points}
         </span>
